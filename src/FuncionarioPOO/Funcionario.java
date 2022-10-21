ac612d31d34cf8f42e81522d265ad5427d87ea81
Heinz Stranner Júnior <Ju-stranner@outlook.com>
2022-10-21 15:13:51 -0300

    foi alteradp a classe Funcionario ,calcularSalario e calculaFerias, tambem alterado mostrarDados do Funcionario e PesquisarFuncionario

--- src/FuncionarioPOO/Funcionario.java
+++ src/FuncionarioPOO/Funcionario.java
@@ -1,86 +1,105 @@
 package FuncionarioPOO;
 
-public class Funcionario{
-	private int codigo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Funcionario {
+	private Integer codigo;
 	private String nome;
 	private double salario;
-	private int vendas;
+	private double vendas;
 	private int ferias;
-	
 
-	public void Funcionario(int codigo,String nome,double salario,int vendas,int ferias) {
-		 this.codigo = codigo;
-		 this.nome = nome;
-		 this.salario = salario;
-		 this.vendas = vendas;
-		 this.ferias = ferias;
+
+	public Funcionario(int codigo2, String nome2, double salario2, double vendas2, int ferias2) {
+		this.codigo = codigo2;
+		this.nome = nome2;
+		this.salario = salario2;
+		this.vendas = vendas2;
+		this.ferias = ferias2;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
-		return "Funcionario [codigo=" + codigo + ", nome=" + nome + ", vendas=" + vendas + ", ferias=" + ferias + "]";
+		return "Funcionario [codigo=" + codigo + ", nome=" + nome + ", salario=" + salario + ", vendas=" + vendas
+				+ ", ferias=" + ferias + "]";
 	}
+
+//pegar a variavel salario += salario * 0.05
 	public double calcularSalario(double comissao) {
-		if(comissao >= 1000 && comissao < 5000) {
-			return salario* 1.05;
-		}else if(comissao >=5000 && comissao <10000) {
-			return salario * 1.10;
-		}else if (comissao >=10000 && comissao < 15000) {
-		    return salario * 1.15;
-		}
-	    return 0.0;
-		
-	}
 	
-	public double calculaFerias(double dias ) {
-		if(dias == 5) {
-		return salario * 0.95;
-		}else if(dias == 10) {
-		return salario * 0.90;
-		}else if(dias == 15) {
-		return salario * 0.85;
+		if (comissao >= 1000 && comissao < 5000) {
+			this.salario = this.salario * 1.05;
+			return salario;
+		} else if (comissao >= 5000 && comissao < 10000) {
+			this.salario = this.salario *  1.10;
+			return salario ;
+		} else if (comissao >= 10000) {
+			this.salario = this.salario * 1.15;
+			return salario ;
 		}
-		return 0;
-		
+		return salario;
+
 	}
-	public int getCodigo() {
+
+	public double calculaFerias(int dias,Funcionario func) {
+		double desconto = 0;
+		if (dias == 5) {
+			 desconto=  func.getSalario() * 0.95;
+			 System.out.println(desconto);
+              return desconto;
+		} else if (dias == 10) {
+			 desconto=func.getSalario() * 0.90;
+			 System.out.println(desconto);
+			 return desconto;
+		} else if (dias == 15) {
+			 desconto= func.getSalario() * 0.85;
+			 System.out.println(desconto);
+			 return desconto;
+		}
+         return desconto;
+	}
+
+	
+
+	public Integer getCodigo() {
 		return codigo;
 	}
-	public void setCodigo(int codigo) {
+
+	public void setCodigo(Integer codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	public int getVendas() {
+
+	public double getVendas() {
 		return vendas;
 	}
-	public void setVendas(int vendas) {
+
+	public void setVendas(double vendas) {
 		this.vendas = vendas;
 	}
+
 	public int getFerias() {
 		return ferias;
 	}
+
 	public void setFerias(int ferias) {
 		this.ferias = ferias;
 	}
 
-	
-
-		
-	
-
-	
 }
